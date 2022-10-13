22ddfe1f947084b13b9889da9a00a465e7a9d1d0
鈴山佳宏 <suzusanyoshihiro@admin.local>
2022-10-13 16:17:13 +0900

    env.local/create

--- src/request.js
+++ src/request.js
@@ -1,12 +1,10 @@
-const API_KEY = "4590af816c62c5cd7f5cf8ec544344b0";
-
 export const requests = {
-  feachTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-  feachNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  feactTopRated: `/discover/tv?api_key=${API_KEY}&languager=en-us`,
-  feactActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
-  feactComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-  feactHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
-  feactRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
-  feactDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+  feachTrending: `/trending/all/week?api_key=${process.env.API_KEY}&language=en-us`,
+  feachNetflixOriginals: `/discover/tv?api_key=${process.env.API_KEY}&with_networks=213`,
+  feactTopRated: `/discover/tv?api_key=${process.env.API_KEY}&languager=en-us`,
+  feactActionMovies: `/discover/tv?api_key=${process.env.API_KEY}&with_genres=28`,
+  feactComedyMovies: `/discover/tv?api_key=${process.env.API_KEY}&with_genres=35`,
+  feactHorrorMovies: `/discover/tv?api_key=${process.env.API_KEY}&with_genres=27`,
+  feactRomanceMovies: `/discover/tv?api_key=${process.env.API_KEY}&with_genres=10749`,
+  feactDocumentMovies: `/discover/tv?api_key=${process.env.API_KEY}&with_genres=99`,
 };
